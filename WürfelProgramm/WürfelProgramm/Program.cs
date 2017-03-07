1c21cddb4c75328cb755ac0f7bff02584f9ae0e8
Dennis W <d.wachtendorf.x@gmail.com>
2017-03-07 20:38:06 +0100

    Add files via upload

---
+++ WürfelProgramm/WürfelProgramm/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WürfelProgramm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            start:
+            Random numberGen = new Random();
+
+            string Name;
+            int Würfel;
+            int i = 0;
+            int Versuch;
+            
+            Console.WriteLine("Hallo, wie ist dein Name?");
+            Name = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Okay " + Name + " wie viele Würfel willst du werfen?");
+            Würfel = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Okay, " + Würfel + " Würfel werden geworfen.");
+            while (i <= Würfel) 
+            {
+                Versuch = numberGen.Next(1, 6);
+                i++;
+                Console.WriteLine("Du hast eine " + Versuch + " gewürfelt.");
+
+            }
+            Console.ReadKey();
+
+            Console.Clear();
+
+            goto start;
+
+            Console.ReadKey();
+        }
+    }
+}
